50b302c9f53ad17e1674252cb94924b4bc0e5265
ShivalThakur <shivalthakur@gmail.com>
2017-10-25 20:01:48 -0700

    Add files via upload

---
+++ EasyDbcl.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for EasyDbcl
+/// </summary>
+public class EasyDbcl
+{
+    SqlConnection oConnection = null;
+    SqlCommand oCommand = null;
+    SqlDataAdapter oDataAdapter = null;
+    SqlDataReader oDataReader = null;
+    DataSet oDs = null;
+    string m_sSql = null;
+    string sConString = ConfigurationSettings.AppSettings["ConnectionString"];
+    public EasyDbcl()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public DataSet GetDataset(SqlCommand sSql)
+    {
+        GetConnection();
+        oDataAdapter = new SqlDataAdapter("", oConnection);
+        sSql.Connection = oConnection;
+        oDataAdapter.SelectCommand = sSql;
+        oDs = new DataSet();
+        try
+        {
+            oDataAdapter.Fill(oDs);
+            CloseConnection();
+        }
+        catch (Exception e)
+        {
+
+            //throw;
+        }
+        finally
+        {
+            CloseConnection();
+        }
+        return oDs;
+    }
+    public void GetConnection()
+    {
+
+        oConnection = new SqlConnection(sConString);
+        oConnection.Open();
+
+    }
+
+    public void CloseConnection()
+    {
+        if (oConnection.State == ConnectionState.Open)
+        {
+            oConnection.Close();
+        }
+    }
+
+    public void ExecuteCommand(string sSql)
+    {
+        GetConnection();
+        oCommand = new SqlCommand(sSql, oConnection);
+        oCommand.ExecuteNonQuery();
+        CloseConnection();
+    }
+    public void ExecuteCommand(SqlCommand sSql)
+    {
+        GetConnection();
+        sSql.Connection = oConnection;
+        oCommand = sSql;
+        oCommand.ExecuteNonQuery();
+        CloseConnection();
+    }
+
+
+    public int ExecuteScalar(string sSql)
+    {
+        int iReturn = 0;
+        GetConnection();
+        oCommand = new SqlCommand(sSql, oConnection);
+        iReturn = Convert.ToInt32(oCommand.ExecuteScalar());
+        CloseConnection();
+        return iReturn;
+    }
+    public int ExecuteScalar(SqlCommand sSql)
+    {
+        int iReturn = 0;
+        GetConnection();
+        sSql.Connection = oConnection;
+        oCommand = sSql;
+        iReturn = Convert.ToInt32(oCommand.ExecuteScalar());
+        CloseConnection();
+        return iReturn;
+    }
+
+    public DataSet GetDataset(string sSql)
+    {
+        GetConnection();
+        oDataAdapter = new SqlDataAdapter(sSql, oConnection);
+        oDataAdapter.SelectCommand.CommandTimeout = 0;
+        oDs = new DataSet();
+        try
+        {
+            oDataAdapter.Fill(oDs);
+            CloseConnection();
+        }
+        catch (Exception e)
+        {
+            Emailcl email = new Emailcl();
+            email.sendMail(e.Message + "<br />" + e.StackTrace + "<br />" + e.Source, "Error Tracker", "shivalthakur@gmail.com", "", "");
+            //throw;
+        }
+        finally
+        {
+            CloseConnection();
+        }
+        return oDs;
+    }
+
+    public SqlDataReader GetDataReader(string sSql)
+    {
+        GetConnection();
+        oCommand = new SqlCommand(sSql, oConnection);
+        oDataReader = oCommand.ExecuteReader();
+        return oDataReader;
+    }
+}
